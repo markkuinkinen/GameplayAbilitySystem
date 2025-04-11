30164d87c028da2358f1b97517045b3a9797be4b
Markku Inkinen <94799551+markkuinkinen@users.noreply.github.com>
2025-04-11 20:58:59 +0300

    Cursor over enemy uses interface correctly

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,12 +4,83 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include <Interaction/EnemyInterface.h>
 
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
 }
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+
+}
+
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, false, CursorHit);
+
+	if (!CursorHit.bBlockingHit) return;
+	
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+	//Cast<IEnemyInterface>(CursorHit.GetActor());	// maybe necessary
+
+	/*
+		Line trace from cursor. There are several scenarios:
+			A: Last actor is null && this actor is null
+				- Do nothing
+			B: LastActor is null && ThisActor is valid
+				- Call highlight on ThisActor
+			C: LastActor is valid && ThisActor is null
+				- Unhighlight LastActor
+			D: Both actors are valid, but LastActor != ThisActor
+				- Unhighlight LastActor && Highlight ThisActor
+			E: Both actors are valid, and are the same actor
+				- Do nothing
+	*/
+
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			// Case B
+			ThisActor->HighlightActor();
+		}
+		else
+		{
+			// Case A: Both are null, Do nothing 
+		}
+	}
+	else // LastActor is valid
+	{
+		if (ThisActor == nullptr)
+		{
+			// Case C:
+			LastActor->UnHighlightActor();
+		}
+		else // Both actors are valid
+		{
+			if (LastActor != ThisActor)
+			{
+				// Case D:
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+			else
+			{
+				// Case E: Both actors valid, Do nothing
+			}
+		}
+	}
+}
+
 
 void AAuraPlayerController::BeginPlay()
 {
