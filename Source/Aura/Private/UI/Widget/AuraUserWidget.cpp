405086dd937ed4e2162836dc97e4ec1e88401d1a
Markku Inkinen <94799551+markkuinkinen@users.noreply.github.com>
2025-04-13 21:01:17 +0300

    Base user widget/Widget controller created

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,11 @@
+// Copyright Markku Inkinen
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
+
