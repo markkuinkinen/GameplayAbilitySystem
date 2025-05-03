fccf1b3ed57c30523df8718c27b4d1c93c18d653
Markku Inkinen <94799551+markkuinkinen@users.noreply.github.com>
2025-05-03 22:32:14 +0300

    Attribute menu controller based on OverlayWidgetController created

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Markku Inkinen
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+
+}
