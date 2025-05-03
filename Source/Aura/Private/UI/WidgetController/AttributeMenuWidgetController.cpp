1d46a263920aca406105952ce856cc190d16a410
Markku Inkinen <94799551+markkuinkinen@users.noreply.github.com>
2025-05-04 00:23:04 +0300

    Set up strength Attribute info delegate for AttributeMenu (tags to come)

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -2,6 +2,11 @@
 
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+#include "AuraGameplayTags.h"
+
+// inform the widget of the values it should change
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
@@ -10,5 +15,12 @@ void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
 
+	check(AttributeInfo);
+
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
+	
+	Info.AttributeValue = AS->GetStrength();
+	AttributeInfoDelegate.Broadcast(Info);
 }
